ded921c530cc3943708fc27e58df6acbaad89a7b
Erik Wahlström <erik93_93@hotmail.com>
2020-01-22 14:28:14 +0100

    deleted navbar component

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,11 @@
 
 <script>
 import Rockets from "./views/Rockets.vue";
-import NavBar from "./components/NavBar.vue";
 
 export default {
   name: "app",
   components: {
     Rockets,
-    NavBar
   }
 };
 </script>
